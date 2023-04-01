7c82a960e08342ee881a55853aa37788f434c191
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2023-04-01 15:25:54 +0530

    Update sqoop-ingestion.py

--- sqoop-ingestion.py
+++ sqoop-ingestion.py
@@ -1,1 +1,18 @@
+# Import data from a MySQL database
+sqoop import \
+--connect jdbc:mysql://localhost/mydatabase \
+--username myuser \
+--password mypassword \
+--table mytable \
+--target-dir /mydirectory \
+--split-by mycolumn \
+--fields-terminated-by '\t'
 
+# Export data to a MySQL database
+sqoop export \
+--connect jdbc:mysql://localhost/mydatabase \
+--username myuser \
+--password mypassword \
+--table mytable \
+--export-dir /mydirectory \
+--input-fields-terminated-by '\t'
