c4685bf08cc086768fe912e155b7c062a4f6b85a
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2023-04-01 15:25:22 +0530

    Update hadoop-cluster-setup.sh

--- hadoop-cluster-setup.sh
+++ hadoop-cluster-setup.sh
@@ -1,1 +1,8 @@
+# Install Ambari
+sudo apt-get update
+sudo apt-get install ambari-server
 
+# Start Ambari Server
+sudo ambari-server start
+
+# Open Ambari Web UI in your browser and follow the instructions to set up your cluster
